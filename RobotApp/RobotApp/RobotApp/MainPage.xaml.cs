7714ce67d1a2d4a9cf07e76038d1512544e9b1e1
Нестеренко Ирина <max.shuldiner777@gmail.com>
2021-09-15 21:26:46 +0300

    Initial commit

---
+++ RobotApp/RobotApp/RobotApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RobotApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Message_Sent(object sender, EventArgs e)
+        {
+            SerialPort serialPort = new SerialPort();
+            serialPort.BaudRate = 115200;
+
+            if (serialPort.IsOpen)
+            {
+                string message = this.Message.Text;
+
+                try
+                {
+                    serialPort.WriteLine(message);
+                    await DisplayAlert("Success", "The message was sent", "Ok");
+                }
+                catch(TimeoutException ex)
+                {
+                    throw ex;
+                }
+            }
+
+            serialPort.Close();
+        }
+    }
+}
